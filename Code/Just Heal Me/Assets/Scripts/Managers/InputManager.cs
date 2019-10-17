25c0dced939603b527e5a6baef10dfe6aa98c64e
David Bates <me@dave-bates.net>
2019-10-17 18:33:01 -0500

    Small Fixes
    Fixes the facing direction reseting when moving forward/backward
    Fixes the framerate issue

--- Code/Just Heal Me/Assets/Scripts/Managers/InputManager.cs
+++ Code/Just Heal Me/Assets/Scripts/Managers/InputManager.cs
@@ -7,6 +7,7 @@ namespace Managers
     public class InputManager : Singleton<InputManager>
     {
         private Player _player;
+        private bool _facingRight = true;
 
         #region -----[ Unity Lifecycle ]---------------------------------------
 
@@ -35,11 +36,21 @@ namespace Managers
         {
             var horizontal = Input.GetAxis("Horizontal");
             var vertical = Input.GetAxis("Vertical");
-
+            
             if (horizontal == 0f && vertical == 0f) return; // no need to do anything
 
+            // we want to maintain our facing direction
+            if (horizontal < 0)
+            {
+                _facingRight = false;
+            }
+            else if (horizontal > 0)
+            {
+                _facingRight = true;
+            }
+            
             // Which way are we running?
-            _player.transform.rotation = Quaternion.Euler(0, horizontal < 0 ? 180 : 0, 0);
+            _player.transform.rotation = Quaternion.Euler(0, _facingRight ? 0 : 180, 0);
             
             var velocity = _player.Speed * new Vector3(horizontal,0,vertical); // Speed (3) is hardcoded for right now
             _player.CharacterController.Move(velocity * Time.deltaTime);
