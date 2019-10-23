7c81e7fdf4f9e67942201c5a21a7f39e4717055c
David Bates <me@dave-bates.net>
2019-10-23 17:26:34 -0500

    Task #3 Updates
    Some updates for Task 3
    - Adds follow script for the camera.  Quick and dirty, we can add lerping/Tweening later if really needed
    - Removed un-needed prefabs
    - updated the Manager scripts to clean up some warnings

--- Code/Just Heal Me/Assets/Scripts/Managers/InputManager.cs
+++ Code/Just Heal Me/Assets/Scripts/Managers/InputManager.cs
@@ -33,7 +33,7 @@ namespace Managers
         {
             var horizontal = Input.GetAxis("Horizontal");
             var vertical = Input.GetAxis("Vertical");
-            
+
             if (horizontal == 0f && vertical == 0f) return; // no need to do anything
 
             // we want to maintain our facing direction
@@ -45,11 +45,11 @@ namespace Managers
             {
                 _facingRight = true;
             }
-            
+
             // Which way are we running?
-            _player.transform.rotation = Quaternion.Euler(0, _facingRight ? 0 : 180, 0);
-            
-            var velocity = _player.Speed * new Vector3(horizontal,0,vertical); // Speed (3) is hardcoded for right now
+            _player.transform.rotation = Quaternion.Euler(_facingRight ? 45 : -45, _facingRight ? 0 : 180, 0);
+
+            var velocity = _player.Speed * new Vector3(horizontal, 0, vertical); // Speed (3) is hardcoded for right now
             _player.CharacterController.Move(velocity * Time.deltaTime);
         }
 
