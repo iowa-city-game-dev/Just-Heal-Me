cdeb8232d380705551a74f7e1ff8ff444b31580c
David Bates <me@dave-bates.net>
2019-10-28 17:32:49 -0500

    Quick Fix #2
    - Adds ESC to exit the game (both in the editor and the app)
    - New ZIP build

--- Code/Just Heal Me/Assets/Scripts/Managers/InputManager.cs
+++ Code/Just Heal Me/Assets/Scripts/Managers/InputManager.cs
@@ -11,33 +11,33 @@ namespace Managers
         private Player _player;
         private bool _facingRight = true;
 
-		#region -----[ Unity Lifecycle ]---------------------------------------
+        #region -----[ Unity Lifecycle ]---------------------------------------
 
-		private void Awake()
-		{
-		}
+        private void Awake()
+        {
+        }
 
-		// Start is called before the first frame update
-		void Start()
+        // Start is called before the first frame update
+        void Start()
         {
             // Find the Player  
             _player = FindObjectOfType<Player>();
             if (_player == null) Debug.LogError("Could not find the player!");
 
-			Unit[] units = FindObjectsOfType<Unit>();
+            Unit[] units = FindObjectsOfType<Unit>();
 
-			for (int i = 0; i < units.Length; i++)
-			{
-				units[i].SetupUnitAngles(CameraAngle);
-			}
+            for (int i = 0; i < units.Length; i++)
+            {
+                units[i].SetupUnitAngles(CameraAngle);
+            }
         }
 
         // Update is called once per frame
         void Update()
         {
             HandleMovement();
-			HandleActions();
-		}
+            HandleActions();
+        }
 
         #endregion
 
@@ -65,43 +65,52 @@ namespace Managers
 
             // Which way are we running?
             _player.transform.rotation = Quaternion.Euler(_facingRight ? CameraAngle : CameraAngle * -1, _facingRight ? 0 : 180, 0);
-			//_player.HealthBarContainer.transform.rotation = Quaternion.Euler(_facingRight ? CameraAngle : CameraAngle * -1, _facingRight ? 180 : 0, 0);
+            //_player.HealthBarContainer.transform.rotation = Quaternion.Euler(_facingRight ? CameraAngle : CameraAngle * -1, _facingRight ? 180 : 0, 0);
 
-			var currentSpeed = isRunning ? _player.Speed * 2f : _player.Speed;
-            var velocity = currentSpeed  * Time.deltaTime * new Vector3(horizontal, 0, vertical);
+            var currentSpeed = isRunning ? _player.Speed * 2f : _player.Speed;
+            var velocity = currentSpeed * Time.deltaTime * new Vector3(horizontal, 0, vertical);
 
 //            velocity.y = _player.transform.position.y; // This needs to be a static position
-                
+
             _player.CharacterController.Move(velocity);
         }
 
-		private void HandleActions()
-		{
-			if (Input.GetMouseButtonDown(0))
-			{
-				Unit clickedUnit = GetClickedUnit();
-				clickedUnit?.ReceiveHeal(_player.GetHealingPower());
-			}
+        private void HandleActions()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Unit clickedUnit = GetClickedUnit();
+                clickedUnit?.ReceiveHeal(_player.GetHealingPower());
+            }
 
-			if (Input.GetMouseButtonDown(1))
-			{
-				// This code is only here to test damaging a unit.  It will eventually be another player ability.
-				Unit clickedUnit = GetClickedUnit();
-				clickedUnit?.TakeDamage(_player.GetAttackPower());
-			}
-		}
+            if (Input.GetMouseButtonDown(1))
+            {
+                // This code is only here to test damaging a unit.  It will eventually be another player ability.
+                Unit clickedUnit = GetClickedUnit();
+                clickedUnit?.TakeDamage(_player.GetAttackPower());
+            }
 
-		private Unit GetClickedUnit()
-		{
-			Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			if (Physics.Raycast(ray, out hit))
-			{
-				return hit.collider.gameObject.GetComponent<Unit>();
-			}
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+            }
+        }
 
-			return null;
-		}
+        private Unit GetClickedUnit()
+        {
+            Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                return hit.collider.gameObject.GetComponent<Unit>();
+            }
+
+            return null;
+        }
 
         #endregion
 
